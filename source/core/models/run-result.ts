b77a05ab543bc32c4787195b82ca7956dea28471
okensu <okensu.inbox@gmail.com>
2026-03-31 00:57:07 +0600

    feat: add model property to RunResult

--- source/core/models/run-result.ts
+++ source/core/models/run-result.ts
@@ -1,3 +1,9 @@
-export class RunResult {
+import { RunResultOptions } from '../types/run-result-options.ts';
 
+export class RunResult {
+  public model: string;
+
+  constructor(options: RunResultOptions) {
+    this.model = options.model;
+  }
 }
--- source/core/utils/run.ts
+++ source/core/utils/run.ts
@@ -2,7 +2,9 @@ import { RunResult } from '../models/run-result.ts';
 import { RunOptions } from '../types/run-options.ts';
 
 export async function run(options: RunOptions): Promise<RunResult> {
-  const result = new RunResult();
+  const result = new RunResult({
+    model: options.model
+  });
 
   return result;
 }
